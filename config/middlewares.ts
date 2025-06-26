86dc81bad1c1f30c08836b281d3e348d6f75e879
Leonardo Araujo <leonardoaraujo.oct@gmail.com>
2025-06-26 14:26:07 -0500

    base project

---
+++ config/middlewares.ts
@@ -0,0 +1,20 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:5173'], // Cambia el puerto si usas otro en SvelteKit
+      methods: ['GET'],
+      headers: '*',
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
