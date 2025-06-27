991b85dcf516cd38dd32bcf5f0f30cd35aa44a11
Leonardo Araujo <leonardoaraujo.oct@gmail.com>
2025-06-26 23:26:16 -0500

    fixes to build strapi

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,30 +1,12 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'https://market-assets.strapi.io',
-            'https://res.cloudinary.com', // Dominio de Cloudinary
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['http://localhost:5173',"https://danleoropa.com"],
+      origin: ['http://localhost:5173',"https://danleoropa.com"], // Cambia el puerto si usas otro en SvelteKit
       methods: ['GET'],
       headers: ['Content-Type', 'Authorization'],
       credentials: false,
